cebee7c332a5c0aea47f287829a1ee85c4cb4b90
fares12358 <fm883254@gmail.com>
2026-02-02 11:37:05 +0200

    mdf: cookies

--- src/utils/sendToken.js
+++ src/utils/sendToken.js
@@ -10,7 +10,8 @@ export const sendToken = (user, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict"
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    maxAge: 7 * 24 * 60 * 60 * 1000
   });
 
   return token;
